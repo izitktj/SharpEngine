19977320fce90413e9e481dfd4a391ec08e7dba9
izitktj <universoreversoizi@gmail.com>
2024-02-06 00:58:53 -0300

    Future documentation

---
+++ Examples/Raycaster (Under development)/Program.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace SharpEngine;
+internal class Program
+{
+
+    static void Main(string[] args)
+	{
+        Program Program = new Program();
+        
+        Program.MainLoop();
+    }
+
+    public void MainLoop()
+    {
+        Buffer bffr = new Buffer(Console.WindowWidth - 1, Console.WindowHeight - 1);
+        EventManager EventManager = new EventManager();
+        InputManager InputManager = new InputManager();
+        GameObject player = new GameObject('#', 16, 16);
+        Player Player = new Player(player, 1);
+
+        EventManager.SubscribeEvents(Player, InputManager);
+
+        while (true) {
+
+            //Player.MovementManager();
+
+            InputManager.KeyManagment();
+
+            Draw(bffr, player);
+
+            bffr.drawBuffer();
+
+            bffr.clear();
+
+            //bffr.changeResolution();
+            //sorry, not working
+
+            Time.calculeDeltaTime();
+        } 
+    }
+
+    static void Draw(Buffer _bffr, GameObject _player)
+    {
+        Raycaster ryc = new Raycaster(_player);
+
+        _bffr.updateQuad(ryc.MapChar, 15, 15, 23, 23);
+
+    	_bffr.updateChar(_player.Char, Math.Clamp(_player.X, 0, _bffr.LimitX - 1), Math.Clamp(_player.Y, 0, _bffr.LimitY - 1));
+
+        _bffr.updateText("Delta time:", 0, 2);
+
+        _bffr.updateText(Convert.ToString(Time.deltaTime), 0, 3);
+
+        _bffr.updateText("PX: " + Convert.ToString(_player.X), 0, 4);
+
+        _bffr.updateText("PY: " + Convert.ToString(_player.Y), 0, 5);
+
+        _bffr.updateText("BX: " + Convert.ToString(_bffr.LimitX), 0, 6);
+
+        _bffr.updateText("BY: " + Convert.ToString(_bffr.LimitY), 0, 7);
+    }
+}
