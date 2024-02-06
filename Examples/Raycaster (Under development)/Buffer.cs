f876d2aa1f09167b555a322d703f198c9c82d7af
izitktj <universoreversoizi@gmail.com>
2024-02-06 15:15:56 -0300

    several test incoming

--- Examples/Raycaster (Under development)/Buffer.cs
+++ Examples/Raycaster (Under development)/Buffer.cs
@@ -3,26 +3,18 @@ using System.Text;
 
 namespace SharpEngine;
 
-public class Buffer
+public static class Buffer
 {
-	public char[,] buffer {get; private set;}
-	public int LimitX {get; private set;}
-	public int LimitY {get; private set;}
+	public static char[,] buffer {get; private set;}
+	public static int LimitX {get; private set;}
+	public static int LimitY {get; private set;}
 
-	public Buffer(int _limitX, int _limitY)
-	{
-		LimitX = _limitX;
-		LimitY = _limitY;
-
-		buffer = new char[_limitX, _limitY];
-	}
-
-	public void updateChar(char _char, double _x, double _y)
+	public static void updateChar(char _char, double _x, double _y)
 	{
 		buffer[MathE.RoundToInt(_x), MathE.RoundToInt(_y)] = _char;
 	}
 
-	public void drawBuffer()
+	public static void drawBuffer()
 	{
 		Console.SetCursorPosition(0, 0);
 
@@ -46,12 +38,12 @@ public class Buffer
 		}
 	}
 
-	public void clear()
+	public static void clear()
 	{
 		Array.Clear(buffer);
 	}
 
-	public void updateText(string _text, int _x = 0, int _y = 0)
+	public static void updateText(string _text, int _x = 0, int _y = 0)
 	{
 		for(int i = 0; i < _text.Length; i++)
 		{
@@ -59,7 +51,7 @@ public class Buffer
 		}
 	}
 
-	public void updateQuad(char[] _quad, int _x, int _y, int _limitX, int _limitY)
+	public static void updateQuad(char[] _quad, int _x, int _y, int _limitX, int _limitY)
 	{
 		int y = _y;
 		int x = _x;
@@ -82,7 +74,7 @@ public class Buffer
 		}
 	}
 
-	public void changeBufferLimit(int _limitX, int _limitY)
+	public static void changeBufferLimit(int _limitX, int _limitY)
 	{
 		LimitX = _limitX;
 		LimitY = _limitY;
@@ -92,18 +84,15 @@ public class Buffer
 		Console.SetCursorPosition(0, 0);
 	}
 
-	public void changeResolution()
+	public static void changeResolution()
 	{
-		int Width = Console.WindowWidth - 1;
-		int Height = Console.WindowHeight - 1;
-
-		if (Console.WindowWidth - 1 == Width || Console.WindowHeight - 1 == Height) return;
-
-   		Width = Console.WindowWidth - 1;
-   		Height = Console.WindowHeight - 1;
+   		int Width = Console.WindowWidth - 1;
+   		int Height = Console.WindowHeight - 1;
 
    		changeBufferLimit(Width, Height);
 
-   		updateText("Changed reso", 3, LimitY - 10);
+   		buffer = new char[Width, Height];
+
+   		Console.SetCursorPosition(0, 0);
 	}
 }
