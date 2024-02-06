b592baad9b002820df6f3c79fc6b0dba4fe5e489
izitktj <universoreversoizi@gmail.com>
2024-02-06 00:06:47 -0300

    Removed Legacy input checker

--- src/Player.cs
+++ src/Player.cs
@@ -13,39 +13,6 @@ public class Player
 		Velocity = _velocity;
 	}
 
-    // InputSide InputManagment()
-    // {
-    // 	ConsoleKeyInfo input = Console.ReadKey(true);
-
-    // 	InputSide Out = new InputSide(0,0);
-
-    // 	switch(input.Key)
-    // 	{
-    // 	  case ConsoleKey.W:
-    // 	    Out.Vertical = -1;
-    // 	    break;
-
-    // 	  case ConsoleKey.S:
-    // 	    Out.Vertical = 1;
-    // 	    break;
-
-    // 	  case ConsoleKey.D:
-    // 	    Out.Horizontal = 1;
-    // 	    break;
-
-    // 	  case ConsoleKey.A:
-    // 	    Out.Horizontal = -1;
-    // 	    break;
-
-    // 	  default:
-    // 	    Out.Horizontal = 0;
-    // 	    Out.Vertical = 0;
-    // 	    break;
-    // 	}
-
-    // 	return Out;
-    // } 
-
     public void MovementManager(InputSide Side)
     {
     	GOPlayer.position(GOPlayer.X, Convert.ToDouble(GOPlayer.Y + Side.Vertical * Velocity));
